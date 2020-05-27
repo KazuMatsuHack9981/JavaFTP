82ce12c3ff7cb0abfe4d9b3df7f333782a87ab8a
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-27 22:43:27 +0900

    simple file transfer to server

---
+++ app/Client.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+public class Client {
+	public static void main(String[] args) throws IOException {
+        InetAddress addr = InetAddress.getByName("localhost");
+        System.out.println("addr = " + addr);
+        
+        Socket socket   = new Socket(addr, 8080);
+        String testfile = "./test.txt";
+
+        try {
+            System.out.println("Connection accepted: " + socket);
+            
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(testfile));
+            BufferedReader in     = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out       = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            
+            while((line = reader.readLine()) != null) {
+                out.println(line);
+            }
+            reader.close();
+        } finally {
+                System.out.println("closing...");
+                socket.close();
+	   }
+	}
+}
