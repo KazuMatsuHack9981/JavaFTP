e83eca9a1d19a216432eeaac3702ad1cc3ee3e1f
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-06-20 04:13:16 +0900

    created status

---
+++ app/Status.java
@@ -0,0 +1,15 @@
+public class Status {
+    //good
+    String success = "200";
+
+    //user error
+    String user_not_found = "300";
+    String incorrect_password = "301";
+
+    //file error
+    String file_not_found = "400";
+    String file_exists = "401";
+
+    //other error
+    String fail = "600";
+}
